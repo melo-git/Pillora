22411c25cc5eab127347618aceafcea54eb2154d
melo-git <terrym.tme@gmail.com>
2025-06-30 23:08:54 +0100

    modified

--- MedScriptOrders/views.py
+++ MedScriptOrders/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+# function to insert any prescription, whether text or file, uploaded by user to the database
+#table Prescription table and then notify staff
+def upload_prescription(request):
+    if request.method =='POST':
